bcf0ce475d8b6ddcaf451c060f28d0a249271c63
Leon <leonivanrodrigo@gmail.com>
2019-11-11 00:20:10 -0300

    Finish shopping component

---
+++ src/components/Main/Shopping/Shopping.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shopping.css';
+
+class Shopping extends React.Component {
+  render(){
+    return (
+    <nav className="container-shopping">
+      <h1>Shopping component</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ab aliquid aspernatur amet quidem laudantium nihil molestias sed obcaecati sint! Veritatis pariatur rerum nesciunt, placeat nostrum id ut beatae quisquam.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ab aliquid aspernatur amet quidem laudantium nihil molestias sed obcaecati sint! Veritatis pariatur rerum nesciunt, placeat nostrum id ut beatae quisquam.
+      </p>
+    </nav>
+    )
+  }
+}
+
+export default Shopping;
