1ce6356e3551c1404b4742a05a0f2c13134e1ee0
Leon <leonivanrodrigo@gmail.com>
2019-11-11 00:23:20 -0300

    Finish contact component

---
+++ src/components/Main/Contact/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Contact.css';
+
+class Contact extends React.Component {
+  render(){
+    return (
+    <nav className="container-contact">
+      <h1>Contact component</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ab aliquid aspernatur amet quidem laudantium nihil molestias sed obcaecati sint! Veritatis pariatur rerum nesciunt, placeat nostrum id ut beatae quisquam.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ab aliquid aspernatur amet quidem laudantium nihil molestias sed obcaecati sint! Veritatis pariatur rerum nesciunt, placeat nostrum id ut beatae quisquam.
+      </p>
+    </nav>
+    )
+  }
+}
+
+export default Contact;
