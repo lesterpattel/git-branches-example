1b4426fa1ad367159f9a71d37f0e260cd248dbb2
Leon <leonivanrodrigo@gmail.com>
2019-11-11 00:13:53 -0300

    Finish abou component.

---
+++ src/components/Main/About/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css';
+
+class About extends React.Component {
+  render(){
+    return (
+    <nav className="container-about">
+      <h1>About component</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ab aliquid aspernatur amet quidem laudantium nihil molestias sed obcaecati sint! Veritatis pariatur rerum nesciunt, placeat nostrum id ut beatae quisquam.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ab aliquid aspernatur amet quidem laudantium nihil molestias sed obcaecati sint! Veritatis pariatur rerum nesciunt, placeat nostrum id ut beatae quisquam.
+      </p>
+    </nav>
+    )
+  }
+}
+
+export default About;
